f716acb0b415c19f59721e0173269bd83202e676
Ortana <126905156+ortanaV2@users.noreply.github.com>
2025-04-03 08:17:30 +0200

    Update control.py

--- bartender/sync_files/control.py
+++ bartender/sync_files/control.py
@@ -41,21 +41,81 @@ def wind(up: bool, speed: float) -> None:
         k.motor(1, -speed)
 
     time.sleep(8 * calibration)
-    k.motor(2, 0)
+    k.off(1)
 
 def wind_test() -> None:
     for i in range(5):
         wind(True, 100)
         wind(False, 100)
 
-k.enable_servos()
-# delta_time_move(1, 1600, 0.001)  # 1600 (magazine on ground)
+def shake_it_baby() -> None:
+    for i in range(4):
+        k.set_servo_position(1, 950)  # Leveled Position when winded up
+        time.sleep(0.1)
+        k.set_servo_position(1, 1250)  # Leveled Position when winded up
+        time.sleep(0.1)
 
-# Collect Drinkpods
-# move(True, True, 50, 3.25)
-# move(True, True, -100, 3)
+def starting_sequence(motor_down_wind: float = 3) -> None:
+    delta_time_move(1, 850, 0.0005)  # make magazine beautifully positioned
+    k.motor(0, -10)
+    time.sleep(2.9)
+    k.off(0)
+    k.motor(1, 100)
+    time.sleep(motor_down_wind)
+    k.off(1)
 
-# wind(True, 100)
+def fill_cups_test() -> None:
+    delta_time_move(1, 450, 0.001)  # default starting position
+    time.sleep(3)  # time to refill drinlpods manually
+    delta_time_move(1, 1250, 0.005)  # Fill cup slowly
+    shake_it_baby()   # why not
 
-# move(True, True, 100, 15)
-k.set_servo_position(1, 750)  # Leveled Position when winded up
+def collect_drinkpods_test() -> None:
+    delta_time_move(1, 1600, 0.001)  # 1600 (magazine on ground)
+    move(True, True, 50, 3.25)
+    # move(True, True, -100, 3)
+
+def grab_cups(correct_cup) -> None:
+    # if targeted cup is index 2 take 0 as secondary cup
+    # if targeted cup is index 1 take 0 as secondary cup
+    if correct_cup == 1 or correct_cup == 2:
+        for i in range(5):
+            move(True, True, 100, 1)
+            move(False, False, 0, 0.2)
+        move(True, True, 100, 3)
+    # if targeted cup is index 0 take 2 as secondary cup
+    pass
+
+def detect_cup() -> int:
+    try:
+        frame, masks, contours = utils.detect_contours()
+        correct_cup, sorted_contours = utils.find_cups(frame, masks, contours)
+        print(correct_cup)
+        print(sorted_contours)
+    except Exception:
+        return 2
+
+    return correct_cup  # cup index (from left to right)
+
+# Todo: Cable-Managment, Check for invalid parts
+if __name__ == "__main__":
+    # Setup: Winding String must be 34cm long at start
+    delta_time_move(1, 1560, 0.001)
+    while True:
+        # print("Light Signal:", k.analog(2))
+        # if k.analog(2) <= 100:  # light starting signal
+            # time.sleep(1) #! DEBUG
+            k.enable_servos()
+            k.set_servo_position(0, 1840)
+            cup_index = detect_cup()
+            time.sleep(1) #! DEBUGs
+            motor_wind_length = 4.75  # 4.75 standard
+            starting_sequence(motor_wind_length)
+            k.set_servo_position(0, 1000)
+            time.sleep(2)
+            grab_cups(cup_index)
+            k.motor(1, -100)
+            time.sleep(motor_wind_length)
+            k.off(1)
+            break
+        # time.sleep(0.1)
